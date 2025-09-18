0cdd576ba45b399d3284d7a8fd82401ef3944b7f
alexdragon14 <hilmihashim472@gmail.com>
2025-09-19 05:38:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('frontend/layout')); // serve HTML, CSS, JS
+app.use(express.static('frontend')); // serve HTML, CSS, JS
 
 // PostgreSQL pool
 const pool = new Pool({
